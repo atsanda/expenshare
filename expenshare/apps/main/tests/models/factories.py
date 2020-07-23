58db43aef211e8e985e892724b09860b3685510f
Tsanda Artyom <tsanda.artyom@gmail.com>
2020-07-23 22:20:47 +0300

    feat_test_factories: user factory and sharelist factory were added, plus test

---
+++ expenshare/apps/main/tests/models/factories.py
@@ -0,0 +1,38 @@
+import factory
+from django.contrib.auth.models import User, Group
+from ...models import Sharelist
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'john{n}')
+    email = factory.Sequence(lambda n: f'lennon{n}@thebeatles.com')
+    password = factory.PostGenerationMethodCall(
+        'set_password', 'johnpassword'
+    )
+
+    @factory.post_generation
+    def has_default_group(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            default_group, _ = Group.objects.get_or_create(
+                name='group'
+            )
+            self.groups.add(default_group)
+
+
+class SharelistFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Sharelist
+
+    name = factory.Sequence(lambda n: f'sharelist{n}')
+
+    @factory.post_generation
+    def users(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            self.users.add(*extracted)
